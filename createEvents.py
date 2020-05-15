0320e24338e60bf01e6a6b0d2e1d66ebc2162df2
Maurice Trammell <m216762@usna.edu>
2020-05-15 03:06:06 -0400

    testing and operational flags set && working for CDO assignment

--- createEvents.py
+++ createEvents.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 import datetime
 from calendar_auth import auth
+from info import getEmail
 
-service = auth()
+#FOR THIS TEST
+from cdo_acdo_schedule import getData
 
-# Call the Calendar API
-event = {
+# sample event as template
+"""event = {
     'summary' : 'Test Input',
     'description' : 'Test description',
     'start' : {
@@ -19,6 +21,48 @@ event = {
     'attendees' : [
         {'email' : 'm216762@usna.edu'},
     ],
+}"""
+
+testing = {
+'summary' : 'TEST INPUT',
+'description' : """THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY
+THIS IS A TEST OF THE AUTONOMOUS ADJUTANT SERGEANT PROGRAM __CDO_ASSIGNMENT__ CAPABILITY""",
 }
-event = service.events().insert(calendarId='primary', body=event, sendNotifications = True).execute()
-print ('Event created: %s' % (event.get('htmlLink')))
+operational = {
+'summary' : 'CDO Watch: ',
+'description' : 'This event was created by the Autonomous Adjutant Sergeant Program: if there are any issues with this event (time, date, assignment, description, etc.) please contact your Company Adjutant',
+}
+def makeCDO(info):
+    # blank event object
+    event = {
+        'summary' : 'Sample CDO: ' + info[1],
+        'description' : testing['description'],
+        'start' : {
+            'date' : info[0],
+        },
+        'end' : {
+            'date' : info[0],
+        },
+        'attendees' : [
+            {'email' : getEmail(info[1])},
+        ],
+    }
+    return event
+
+def addEvent(event):
+    service = auth() # authenticate and get the service object
+    # Call the Calendar API
+    event = service.events().insert(calendarId='primary', body=event, sendNotifications = True).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
+if 1 == 1:
+    data = getData()
+    # print(data)
+    for watch in data:
+        # print(watch)
+        addEvent(makeCDO(watch))
